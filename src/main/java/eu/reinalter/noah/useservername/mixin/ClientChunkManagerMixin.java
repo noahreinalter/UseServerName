19ffbb491af6dae437d45bc0a1f5c5a66dfd635d
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2023-12-13 22:37:14 +0100

    Fix mixin load order

--- src/main/java/eu/reinalter/noah/useservername/mixin/ClientChunkManagerMixin.java
+++ src/main/java/eu/reinalter/noah/useservername/mixin/ClientChunkManagerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientChunkManager.class)
+@Mixin(value = ClientChunkManager.class, priority = 500)
 public class ClientChunkManagerMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
     private void useServerNameInit(ClientWorld world, int loadDistance, CallbackInfo ci) {
