d29d3f958ba46f775b4a2b282b854ac020d5f0de
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2024-01-18 19:11:38 +0100

    Fix 1.20 and 1.20.1 Versions not working

--- src/main/java/eu/reinalter/noah/useservername/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/eu/reinalter/noah/useservername/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,10 +1,12 @@
 package eu.reinalter.noah.useservername.mixin;
 
+import com.mojang.authlib.GameProfile;
 import eu.reinalter.noah.useservername.client.UseServerNameClient;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientConnectionState;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.util.telemetry.WorldSession;
 import net.minecraft.network.ClientConnection;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ClientPlayNetworkHandlerMixin {
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void clientPlayNetworkHandlerInit(MinecraftClient client, ClientConnection clientConnection, ClientConnectionState clientConnectionState, CallbackInfo ci) {
-        ServerInfo serverInfo = ((ClientPlayNetworkHandler)(Object)this).getServerInfo();
+    private void clientPlayNetworkHandlerInit(MinecraftClient client, Screen screen, ClientConnection connection, ServerInfo serverInfo, GameProfile profile, WorldSession worldSession, CallbackInfo ci) {
         if (serverInfo != null) {
             UseServerNameClient.getInstance().setServerId(serverInfo.name);
         }
