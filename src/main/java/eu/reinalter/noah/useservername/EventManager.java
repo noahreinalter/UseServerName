f00f55877d19d5aebf103bf7ae1ff8cb28a1d241
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2024-02-03 16:55:05 +0100

    Cleanup EventManager.java

--- src/main/java/eu/reinalter/noah/useservername/EventManager.java
+++ src/main/java/eu/reinalter/noah/useservername/EventManager.java
@@ -4,13 +4,10 @@ import eu.reinalter.noah.useservername.client.UseServerNameClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.loader.api.FabricLoader;
 
 @Environment(EnvType.CLIENT)
 public class EventManager {
     public EventManager() {
-        ClientPlayConnectionEvents.DISCONNECT.register(((handler, client) -> {
-            UseServerNameClient.getInstance().clearServerId();
-        }));
+        ClientPlayConnectionEvents.DISCONNECT.register(((handler, client) -> UseServerNameClient.getInstance().clearServerId()));
     }
 }
