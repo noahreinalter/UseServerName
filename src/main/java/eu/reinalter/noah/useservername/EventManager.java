3684d0a08e846e05ae3233b49bf0b3e56e0e4046
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2024-02-04 14:55:46 +0100

    Use ClientPlayConnectionEvents.Init instead of mixin

--- src/main/java/eu/reinalter/noah/useservername/EventManager.java
+++ src/main/java/eu/reinalter/noah/useservername/EventManager.java
@@ -4,10 +4,17 @@ import eu.reinalter.noah.useservername.client.UseServerNameClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.network.ServerInfo;
 
 @Environment(EnvType.CLIENT)
 public class EventManager {
     public EventManager() {
         ClientPlayConnectionEvents.DISCONNECT.register(((handler, client) -> UseServerNameClient.getInstance().clearServerId()));
+        ClientPlayConnectionEvents.INIT.register(((handler, client) -> {
+            ServerInfo serverInfo = handler.getServerInfo();
+            if (serverInfo != null) {
+                UseServerNameClient.getInstance().setServerId(serverInfo.name, false);
+            }
+        }));
     }
 }
