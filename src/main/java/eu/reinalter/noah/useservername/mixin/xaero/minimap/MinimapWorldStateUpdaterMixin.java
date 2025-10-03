9a574c3b90981b0e5e42298a970fa9db7e879e84
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2025-10-03 18:10:37 +0200

    Update to 1.21.9

--- src/main/java/eu/reinalter/noah/useservername/mixin/xaero/minimap/MinimapWorldStateUpdaterMixin.java
+++ src/main/java/eu/reinalter/noah/useservername/mixin/xaero/minimap/MinimapWorldStateUpdaterMixin.java
@@ -3,20 +3,18 @@ package eu.reinalter.noah.useservername.mixin.xaero.minimap;
 import eu.reinalter.noah.useservername.UseServerName;
 import eu.reinalter.noah.useservername.UseServerNameConfig;
 import eu.reinalter.noah.useservername.client.UseServerNameClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import xaero.hud.minimap.module.MinimapSession;
 import xaero.hud.minimap.world.state.MinimapWorldStateUpdater;
 import xaero.hud.path.XaeroPath;
 
 @Mixin(MinimapWorldStateUpdater.class)
 public abstract class MinimapWorldStateUpdaterMixin {
-    @Inject(method = "getAutoRootContainerPath(ILnet/minecraft/client/network/ClientPlayNetworkHandler;Lxaero/hud/minimap/module/MinimapSession;)Lxaero/hud/path/XaeroPath;", at = @At("RETURN"), cancellable = true)
-    private void convertWorldDimFilesToFolders(int version, ClientPlayNetworkHandler connection, MinimapSession session, CallbackInfoReturnable<XaeroPath> cir) {
+    @Inject(method = "getAutoRootContainerPath(I)Lxaero/hud/path/XaeroPath;", at = @At("RETURN"), cancellable = true, remap = false)
+    private void getAutoRootContainerPath(int version, CallbackInfoReturnable<XaeroPath> cir) {
         if (UseServerNameConfig.HANDLER.instance().xaero_minimap) {
             XaeroPath oldReturnValue = cir.getReturnValue();
             Logger logger = UseServerName.getInstance().logger();
